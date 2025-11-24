b472126fcd77f86a52c9624a8bfa50dd93b4b698
Abicodejr <abijrcoltd@gmail.com>
2025-11-24 12:59:11 +0700

    update border radius for film schedule container

--- lib/pages/film_list_page.dart
+++ lib/pages/film_list_page.dart
@@ -153,6 +153,7 @@ class FilmListPage extends StatelessWidget {
                                     color: Colors.white,
                                     width: 1,
                                   ),
+                                  borderRadius: BorderRadius.circular(20),
                                 ),
                                 child: Text(
                                   schedule,
